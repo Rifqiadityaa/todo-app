9aaf16af2ae3e7ca253f3b48dd74f12251469a99
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-07-21 17:57:20 +0700

    feat: create filter menu

--- src/components/MenuDropdown/index.jsx
+++ src/components/MenuDropdown/index.jsx
@@ -1,22 +1,87 @@
+import { useTodoContext } from "@/shared/context/todoContext";
 import {
   IconButton,
   Menu,
   MenuButton,
+  MenuDivider,
+  MenuGroup,
   MenuItem,
   MenuList,
 } from "@chakra-ui/react";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { FaFilter } from "react-icons/fa6";
 
 const MenuDropdown = () => {
+  const { getItemsData } = useTodoContext();
+
   return (
     <Menu>
       <MenuButton
         as={IconButton}
-        icon={<BsThreeDotsVertical />}
+        icon={<FaFilter color="white" />}
         variant="ghost"
+        _hover={{
+          bg: "#D5CCFF",
+        }}
+        _active={{
+          bg: "#D5CCFF",
+        }}
       />
       <MenuList zIndex={999}>
-        <MenuItem>New Tab</MenuItem>
+        <MenuItem
+          onClick={() => {
+            getItemsData();
+          }}
+        >
+          Show All
+        </MenuItem>
+        <MenuDivider />
+        <MenuGroup title="Priority">
+          <MenuItem
+            onClick={() => {
+              getItemsData({ priority: "High" });
+            }}
+          >
+            High Priority
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              getItemsData({ priority: "Medium" });
+            }}
+          >
+            Medium Priority
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              getItemsData({ priority: "Low" });
+            }}
+          >
+            Low Priority
+          </MenuItem>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup title="Group">
+          <MenuItem
+            onClick={() => {
+              getItemsData({ group: "Development" });
+            }}
+          >
+            Development
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              getItemsData({ group: "Testing" });
+            }}
+          >
+            Testing
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              getItemsData({ group: "Design" });
+            }}
+          >
+            Design
+          </MenuItem>
+        </MenuGroup>
       </MenuList>
     </Menu>
   );
